6c9d8c5483be72e5672ae348dc5267bb2403eb4e
Michael Treser <michael.treser@waycaretech.com>
2023-02-13 13:55:00 +0200

    [Vuetify]: Added v-autocomplete component.

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,5 +2,12 @@
   <h2>Form Component</h2>
   <form>
     <input type="text" placeholder="Place enter text">
+
+    <v-autocomplete
+        clearable
+        label="Autocomplete"
+        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+        multiple
+    ></v-autocomplete>
   </form>
 </template>
