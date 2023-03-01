440e4c52e03fc838583f86ccefbc7dfc97e2a17c
Michael Treser <michael.treser@waycaretech.com>
2023-03-01 11:24:53 +0200

    fix(boardgame component): Added BoardGame component.

---
+++ src/components/BoardGame.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BoardGame",
+  inject: ['boardGamesAmount'],
+  data() {
+    return {
+      totalAmount: this.boardGamesAmount,
+    };
+  },
+  mounted() {
+    console.log('total amount received: ', this.totalAmount)
+  },
+});
+</script>
+
+<template>
+  <div>{{ totalAmount }}</div>
+</template>
+
+<style scoped>
+
+</style>
