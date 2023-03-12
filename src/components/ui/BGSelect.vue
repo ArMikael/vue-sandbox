2c67b411501d95c51d180063bba792c68765e7c9
Michael Treser <armikael2@gmail.com>
2023-03-12 13:04:03 +0200

    feat(bg select): Added new ui component - select to work with dynamic data.

---
+++ src/components/ui/BGSelect.vue
@@ -0,0 +1,25 @@
+<template>
+  <select v-model="modelValue" @change="changeOption">
+    <option
+      v-for="option in optionsList"
+      :key="option.value"
+      :value="option.value">
+      {{ option.name }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "bg-select",
+  methods: {
+    changeOption(event) {
+      this.$emit('update:modelValue', event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
