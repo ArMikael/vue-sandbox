65103bb4efc25253d8f965e9038af04bc1efc8a4
Michael <mtreser@rekor.ai>
2023-03-13 12:56:51 +0200

    feat(bg select): Changed to TS. bg-select implemented in ItemsList.

--- src/components/ui/BGSelect.vue
+++ src/components/ui/BGSelect.vue
@@ -1,25 +1,33 @@
 <template>
   <select v-model="modelValue" @change="changeOption">
-    <option
-      v-for="option in optionsList"
-      :key="option.value"
-      :value="option.value">
+    <option disabled value="">Sort type</option>
+    <option v-for="option in optionsList" :key="option.value" :value="option.value">
       {{ option.name }}
     </option>
   </select>
 </template>
 
-<script>
-export default {
-  name: "bg-select",
-  methods: {
-    changeOption(event) {
-      this.$emit('update:modelValue', event.target.value);
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'bg-select',
+  props: {
+    modelValue: {
+      type: String,
+    },
+    optionsList: {
+      type: Array,
+      default: () => [],
     },
   },
-};
+  methods: {
+    changeOption(event: Event) {
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit('update:modelValue', eventTarget.value);
+    },
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped lang="scss"></style>
