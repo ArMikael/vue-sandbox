ac8e4dc14d9356c1940b982fa7481f7b681499f2
Michael <mtreser@rekor.ai>
2023-03-18 14:05:45 +0200

    fix(items list): Added search field for computed list. Created TextInput component.

---
+++ src/components/ui/TextInput.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { PropType } from 'vue';
+
+export default defineComponent({
+  name: 'TextInput',
+  data() {
+    return {
+      modelValue: this.searchQuery,
+    };
+  },
+  props: {
+    placeholder: {
+      type: String,
+    },
+    searchQuery: {
+      type: Object as PropType<string>,
+    },
+  },
+  methods: {
+    inputChanged(event: Event) {
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit('update:modelValue', eventTarget?.value);
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="text-input">
+    <input type="text" v-model="modelValue" @input="inputChanged" :placeholder="this.placeholder" />
+  </div>
+</template>
+
+<style lang="scss"></style>
