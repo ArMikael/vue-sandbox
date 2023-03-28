d7fd90eeeff90678dd5ba5eae25b42007758bbdb
Michael <mtreser@rekor.ai>
2023-03-28 17:30:50 +0300

    feat(posts list): Implemented pagination flow and layout.

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -7,9 +7,15 @@ export default defineComponent({
   name: 'PostsList',
   data(): {
     postsList: Array<Post>;
+    page: number;
+    limit: number;
+    totalPages: number;
   } {
     return {
       postsList: [],
+      page: 1,
+      limit: 10,
+      totalPages: 0,
     };
   },
   mounted() {
@@ -18,12 +24,22 @@ export default defineComponent({
   methods: {
     async getPosts() {
       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts/');
-        console.log(response);
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts/', {
+          params: {
+            _page: this.page,
+            _limit: this.limit,
+          },
+        });
+
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
         this.postsList = response.data;
       } catch (error) {
         console.error('getPosts error: ', error);
       }
+    },
+    goToPage(pageNumber: number) {
+      this.page = pageNumber;
+      this.getPosts();
     },
   },
 });
@@ -32,11 +48,52 @@ export default defineComponent({
 <template>
   <div class="posts-list-component">
     <ul class="post-list">
-      <li class="post-item" v-for="post in postsList" :key="post.id">
-        {{ post.title }}
-      </li>
+      <li class="post-item" v-for="post in postsList" :key="post.id">{{ post.id }} - {{ post.title }}</li>
     </ul>
+
+    <div class="pages-nav-panel">
+      <ul class="page-controls-list">
+        <li
+          class="page-control"
+          v-for="pageNumber of totalPages"
+          :key="pageNumber"
+          :class="{
+            'page-control--active': pageNumber === page,
+          }"
+          @click="goToPage(pageNumber)">
+          {{ pageNumber }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.post-list {
+  list-style-type: none;
+}
+
+.pages-nav-panel {
+  margin-top: 40px;
+}
+
+.page-controls-list {
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+}
+
+.page-control {
+  width: 30px;
+  margin-right: 4px;
+  text-align: center;
+  border: 1px solid gray;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &--active {
+    border: 1px solid cornflowerblue;
+    background-color: lightskyblue;
+  }
+}
+</style>
